ed88c549454944a1d070df6c4b66e93563891d32
Sorin Costiuc <110764597+SorinCostiuc@users.noreply.github.com>
2023-01-14 10:33:26 +0200

    creating controller and dto

---
+++ src/main/java/com/sda/company/controller/CompanyController.java
@@ -0,0 +1,19 @@
+package com.sda.company.controller;
+
+import com.sda.company.dto.CompanyCreateDto;
+import com.sda.company.dto.CompanyInfoDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/company")
+public class CompanyController {
+
+    @PostMapping("/create")
+    public ResponseEntity<CompanyInfoDto> createCompany(@RequestBody CompanyCreateDto companyCreateDto) {
+        return ResponseEntity.ok(null);
+    }
+}
